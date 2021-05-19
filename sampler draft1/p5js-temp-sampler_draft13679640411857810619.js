d1b4508b392d7ed960c48175811558c3e371fd81
lilsinclair <83621361+lilsinclair@users.noreply.github.com>
2021-05-19 23:11:44 +1000

    Update and rename p5js-temp-sampler_draft13679640411857810619.js to sampler draft1/p5js-temp-sampler_draft13679640411857810619.js

--- sampler draft1/p5js-temp-sampler_draft13679640411857810619.js
+++ sampler draft1/p5js-temp-sampler_draft13679640411857810619.js
@@ -1,6 +1,6 @@
 var font1, rand;
-//const letters= '<=>';
-//var listLength ;
+//const letters= '<=>';//ignore
+//var listLength ;//ignore
 
 
 
@@ -16,17 +16,17 @@ background(252,200,200);
 textSize(50);
 textFont(font1);
 textAlign(CENTER,CENTER);
-//listLength = letters.length ;
+//listLength = letters.length ;//ignore this
 
 }
 
 function draw() {
   //rand = int(random(0,listLength));
-  background(255,200,200);//opacity 35
+  background(255,200,200);
    fill(mouseX/3,mouseY/2,200);
    text('<',mouseX, mouseY);
    text('=',pmouseX,pmouseY);
-   //text('>',pmouseX,pmouseY);
+   //text('>',pmouseX,pmouseY); //I want this glyph to follow third in line
 frameRate(15);
 
 }
