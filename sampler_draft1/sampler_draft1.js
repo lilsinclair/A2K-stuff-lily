c1e7db99cecc0e3f452d20628c720faa40a27d69
lilsinclair <83621361+lilsinclair@users.noreply.github.com>
2021-05-20 18:17:45 +1000

    Delete sampler_draft1.js

--- sampler_draft1/sampler_draft1.js
+++
@@ -1,48 +0,0 @@
-var font, fontSize, testText;
-
-
-function preload(){
-font=loadFont('data/playschool.otf');
-}
-
-function setup() {
- createCanvas(windowWidth, windowHeight);
- background(252,200,200);
- textSize(50);
- textFont(font);
- textAlign(CENTER,CENTER);
-//listLength = letters.length ;
-fill(250,0,0);
-noStroke();
-textFont(font);
-fontSize=(height/2);
-testText="{hiya}";
-}
-
-function draw() {
-  background(255,200,200);
-textSize(fontSize);
-text(testText, width/2, height/2);
-
-
-push();
-   fill(mouseX/3,mouseY/2,200);
-   textSize(50);
-   text('<',mouseX, mouseY);
-   text('=',pmouseX,pmouseY);
-   //text('>',pmouseX,pmouseY);//I wanted this glyph to follow third in the little mouse train
-   frameRate(15);
-pop();
-}
-
-function keyTyped() {
-testText=key;
-background(255,200,200);
-}
-
-
-function keyPressed(){
- {   if (key===' '){     
- background(252,200,200);   }
-  } 
-}
