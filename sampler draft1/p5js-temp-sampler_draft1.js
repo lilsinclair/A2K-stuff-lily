810dbaec887ca0c8edbc1388869d61abf425a413
lilsinclair <83621361+lilsinclair@users.noreply.github.com>
2021-05-20 17:55:38 +1000

    Delete p5js-temp-sampler_draft1.js

--- sampler draft1/p5js-temp-sampler_draft1.js
+++
@@ -1,39 +0,0 @@
-var font1, rand;
-//const letters= '<=>';//ignore
-//var listLength ;//ignore
-
-
-
-
-function preload(){
-font1=loadFont('data/playschool.otf');
-
-}
-
-function setup() {
- createCanvas(windowWidth, windowHeight);
-background(252,200,200);
-textSize(50);
-textFont(font1);
-textAlign(CENTER,CENTER);
-//listLength = letters.length ;//ignore this
-
-}
-
-function draw() {
-  //rand = int(random(0,listLength));
-  background(255,200,200);
-   fill(mouseX/3,mouseY/2,200);
-   text('<',mouseX, mouseY);
-   text('=',pmouseX,pmouseY);
-   //text('>',pmouseX,pmouseY); //I want this glyph to follow third in line
-frameRate(15);
-
-}
-
-
-function keyPressed(){
- {   if (key===' '){     
- background(252,200,200);   }
-  } 
-}
